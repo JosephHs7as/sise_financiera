67f142e9394b7de472ef2600cdc202fd164d6fde
JosephHs7as <pradoJoseph79@gmail.com>
2025-10-06 15:40:36 -0500

    Delete Financiera.Model directory

--- Financiera.Model/TipoCliente.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Financiera.Model
-{
-    public class TipoCliente
-    {
-        public int ID { get; set; }
-        public string Nombre { get; set; }
-        public bool Activo { get; set; }
-    }
-}
